e2870eba2bb2ee38dbe9eefc78008bd14cdef770
Joffrey <joffrey.mischlerps@gmail.com>
2024-01-11 22:27:36 +0100

    implement firebase in texteditor

--- src/components/TextEditor.vue
+++ src/components/TextEditor.vue
@@ -1,115 +1,127 @@
 <script lang="ts">
 import {defineComponent} from 'vue'
-
-import {app} from '@/firebase/init';
-import {getDatabase, set, ref, push, query, limitToLast, serverTimestamp} from "firebase/database";
-import type {Message} from "@/components/message";
+import {getDatabase, push, ref, serverTimestamp, set} from "firebase/database";
+import {app} from "@/firebase/init";
 
 const db = getDatabase(app);
 const messagesRef = ref(db, 'messages/');
 
 export default defineComponent({
   name: "TextEditor",
+  props: {
+    username: String
+  },
   data() {
     return {
       message: '',
     }
   },
   methods: {
-    emitMessage(){
-      this.$emit("message", this.message)
+    sendMessage() {
+      if (this.message === "") {
+        return;
+      }
+
+      set(push(messagesRef), {
+        username: this.username,
+        message: this.message,
+        timestamp: serverTimestamp(),
+      });
+      this.message = "";
     }
   }
 })
 </script>
 
 <template>
+  <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"/>
   <!--
   // v0 by Vercel.
   // https://v0.dev/t/4D5CdhA
   -->
-  <div class="w-[600px] max-w-[800px] bg-white dark:bg-gray-800 shadow-lg rounded-lg overflow-hidden">
+  <div class="w-full max-w-[60rem] mx-5 bg-white dark:bg-gray-800 shadow-lg rounded-lg overflow-hidden">
     <div class="flex space-x-2 p-2 border-b border-gray-200 dark:border-gray-800">
-<!--      <button-->
-<!--          class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 text-gray-600 dark:text-gray-400">-->
-<!--        <span class="sr-only">Bold</span>-->
-<!--        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"-->
-<!--             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4">-->
-<!--          <path d="M14 12a4 4 0 0 0 0-8H6v8"></path>-->
-<!--          <path d="M15 20a4 4 0 0 0 0-8H6v8Z"></path>-->
-<!--        </svg>-->
-<!--      </button>-->
-<!--      <button-->
-<!--          class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 text-gray-600 dark:text-gray-400">-->
-<!--        <span class="sr-only">Italic</span>-->
-<!--        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"-->
-<!--             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4">-->
-<!--          <line x1="19" x2="10" y1="4" y2="4"></line>-->
-<!--          <line x1="14" x2="5" y1="20" y2="20"></line>-->
-<!--          <line x1="15" x2="9" y1="4" y2="20"></line>-->
-<!--        </svg>-->
-<!--      </button>-->
-<!--      <button-->
-<!--          class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 text-gray-600 dark:text-gray-400">-->
-<!--        <span class="sr-only">Underline</span>-->
-<!--        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"-->
-<!--             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4">-->
-<!--          <path d="M6 4v6a6 6 0 0 0 12 0V4"></path>-->
-<!--          <line x1="4" x2="20" y1="20" y2="20"></line>-->
-<!--        </svg>-->
-<!--      </button>-->
-<!--      <button-->
-<!--          class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 text-gray-600 dark:text-gray-400">-->
-<!--        <span class="sr-only">Left Align</span>-->
-<!--        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"-->
-<!--             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4">-->
-<!--          <line x1="21" x2="3" y1="6" y2="6"></line>-->
-<!--          <line x1="15" x2="3" y1="12" y2="12"></line>-->
-<!--          <line x1="17" x2="3" y1="18" y2="18"></line>-->
-<!--        </svg>-->
-<!--      </button>-->
-<!--      <button-->
-<!--          class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 text-gray-600 dark:text-gray-400">-->
-<!--        <span class="sr-only">Center Align</span>-->
-<!--        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"-->
-<!--             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4">-->
-<!--          <line x1="21" x2="3" y1="6" y2="6"></line>-->
-<!--          <line x1="17" x2="7" y1="12" y2="12"></line>-->
-<!--          <line x1="19" x2="5" y1="18" y2="18"></line>-->
-<!--        </svg>-->
-<!--      </button>-->
-<!--      <button-->
-<!--          class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 text-gray-600 dark:text-gray-400">-->
-<!--        <span class="sr-only">Right Align</span>-->
-<!--        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"-->
-<!--             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4">-->
-<!--          <line x1="21" x2="3" y1="6" y2="6"></line>-->
-<!--          <line x1="21" x2="9" y1="12" y2="12"></line>-->
-<!--          <line x1="21" x2="7" y1="18" y2="18"></line>-->
-<!--        </svg>-->
-<!--      </button>-->
-<!--      <button-->
-<!--          class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 text-gray-600 dark:text-gray-400">-->
-<!--        <span class="sr-only">Bulleted List</span>-->
-<!--        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"-->
-<!--             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4">-->
-<!--          <line x1="8" x2="21" y1="6" y2="6"></line>-->
-<!--          <line x1="8" x2="21" y1="12" y2="12"></line>-->
-<!--          <line x1="8" x2="21" y1="18" y2="18"></line>-->
-<!--          <line x1="3" x2="3.01" y1="6" y2="6"></line>-->
-<!--          <line x1="3" x2="3.01" y1="12" y2="12"></line>-->
-<!--          <line x1="3" x2="3.01" y1="18" y2="18"></line>-->
-<!--        </svg>-->
-<!--      </button>-->
-<!--      <button-->
-<!--          class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 text-gray-600 dark:text-gray-400">-->
-<!--        <span class="sr-only">Add Link</span>-->
-<!--        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"-->
-<!--             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4">-->
-<!--          <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path>-->
-<!--          <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path>-->
-<!--        </svg>-->
-<!--      </button>-->
+      <!--      <button-->
+      <!--          class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 text-gray-600 dark:text-gray-400">-->
+      <!--        <span class="sr-only">Bold</span>-->
+      <!--        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"-->
+      <!--             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4">-->
+      <!--          <path d="M14 12a4 4 0 0 0 0-8H6v8"></path>-->
+      <!--          <path d="M15 20a4 4 0 0 0 0-8H6v8Z"></path>-->
+      <!--        </svg>-->
+      <!--      </button>-->
+      <!--      <button-->
+      <!--          class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 text-gray-600 dark:text-gray-400">-->
+      <!--        <span class="sr-only">Italic</span>-->
+      <!--        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"-->
+      <!--             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4">-->
+      <!--          <line x1="19" x2="10" y1="4" y2="4"></line>-->
+      <!--          <line x1="14" x2="5" y1="20" y2="20"></line>-->
+      <!--          <line x1="15" x2="9" y1="4" y2="20"></line>-->
+      <!--        </svg>-->
+      <!--      </button>-->
+      <!--      <button-->
+      <!--          class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 text-gray-600 dark:text-gray-400">-->
+      <!--        <span class="sr-only">Underline</span>-->
+      <!--        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"-->
+      <!--             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4">-->
+      <!--          <path d="M6 4v6a6 6 0 0 0 12 0V4"></path>-->
+      <!--          <line x1="4" x2="20" y1="20" y2="20"></line>-->
+      <!--        </svg>-->
+      <!--      </button>-->
+      <!--      <button-->
+      <!--          class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 text-gray-600 dark:text-gray-400">-->
+      <!--        <span class="sr-only">Left Align</span>-->
+      <!--        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"-->
+      <!--             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4">-->
+      <!--          <line x1="21" x2="3" y1="6" y2="6"></line>-->
+      <!--          <line x1="15" x2="3" y1="12" y2="12"></line>-->
+      <!--          <line x1="17" x2="3" y1="18" y2="18"></line>-->
+      <!--        </svg>-->
+      <!--      </button>-->
+      <!--      <button-->
+      <!--          class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 text-gray-600 dark:text-gray-400">-->
+      <!--        <span class="sr-only">Center Align</span>-->
+      <!--        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"-->
+      <!--             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4">-->
+      <!--          <line x1="21" x2="3" y1="6" y2="6"></line>-->
+      <!--          <line x1="17" x2="7" y1="12" y2="12"></line>-->
+      <!--          <line x1="19" x2="5" y1="18" y2="18"></line>-->
+      <!--        </svg>-->
+      <!--      </button>-->
+      <!--      <button-->
+      <!--          class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 text-gray-600 dark:text-gray-400">-->
+      <!--        <span class="sr-only">Right Align</span>-->
+      <!--        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"-->
+      <!--             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4">-->
+      <!--          <line x1="21" x2="3" y1="6" y2="6"></line>-->
+      <!--          <line x1="21" x2="9" y1="12" y2="12"></line>-->
+      <!--          <line x1="21" x2="7" y1="18" y2="18"></line>-->
+      <!--        </svg>-->
+      <!--      </button>-->
+      <!--      <button-->
+      <!--          class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 text-gray-600 dark:text-gray-400">-->
+      <!--        <span class="sr-only">Bulleted List</span>-->
+      <!--        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"-->
+      <!--             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4">-->
+      <!--          <line x1="8" x2="21" y1="6" y2="6"></line>-->
+      <!--          <line x1="8" x2="21" y1="12" y2="12"></line>-->
+      <!--          <line x1="8" x2="21" y1="18" y2="18"></line>-->
+      <!--          <line x1="3" x2="3.01" y1="6" y2="6"></line>-->
+      <!--          <line x1="3" x2="3.01" y1="12" y2="12"></line>-->
+      <!--          <line x1="3" x2="3.01" y1="18" y2="18"></line>-->
+      <!--        </svg>-->
+      <!--      </button>-->
+      <!--      <button-->
+      <!--          class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 text-gray-600 dark:text-gray-400">-->
+      <!--        <span class="sr-only">Add Link</span>-->
+      <!--        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"-->
+      <!--             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4">-->
+      <!--          <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path>-->
+      <!--          <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path>-->
+      <!--        </svg>-->
+      <!--      </button>-->
       <button
           class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 text-gray-600 dark:text-gray-400">
         <span class="sr-only">Image</span>
@@ -132,9 +144,16 @@ export default defineComponent({
         </svg>
       </button>
     </div>
-    <textarea
-        class="w-full h-full p-4 text-gray-600 dark:text-gray-200 dark:bg-gray-700 text-md resize-none outline-none"
-        placeholder="Type your text here..."></textarea>
+    <div class="overflow-y-auto relative">
+      <div class="absolute bottom-0 right-3">
+        <button @click="sendMessage"><span class="material-symbols-outlined text-gray-600 dark:text-gray-400">send</span>
+        </button>
+      </div>
+      <input
+          class="w-full h-full p-6 py-8 text-gray-600 dark:text-gray-200 dark:bg-gray-700 text-md resize-none outline-none overflow-y-auto"
+          placeholder="Type your text here..." maxlength="250" v-model="message"/>
+    </div>
+
   </div>
 </template>
 
