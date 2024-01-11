3748ff03d1f36a3344f2e8fd8ec133dd36a364d7
Joffrey <joffrey.mischlerps@gmail.com>
2024-01-11 22:46:57 +0100

    fix name

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import {createRouter, createWebHistory} from 'vue-router'
-import ChatView2 from "@/views/ChatView2.vue";
+import ChatView from "@/views/ChatView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
         {
             path: '/',
             name: 'main',
-            component: ChatView2
+            component: ChatView
         },
         // {
         //   path: '/about',
