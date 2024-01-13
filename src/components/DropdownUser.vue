cd3f268a1556b4f982fdd44b4d9eb9de0fbc250b
Joffrey <joffrey.mischlerps@gmail.com>
2024-01-13 22:08:11 +0100

    fix dropdown button

--- src/components/DropdownUser.vue
+++ src/components/DropdownUser.vue
@@ -36,11 +36,11 @@ export default defineComponent({
     </button>
 
     <!-- Dropdown menu -->
-    <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+    <div id="dropdown" class="z-10 hidden  bg-white divide-y divide-gray-100 rounded-lg shadow w-32 dark:bg-gray-700">
       <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
         <li>
           <a @click="disconnectUser"
-             class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Se déconnecter</a>
+             class="block px-4 py-2 cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Se déconnecter</a>
         </li>
       </ul>
     </div>
